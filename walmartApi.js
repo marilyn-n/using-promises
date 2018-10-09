31cd5515ea98471a14f1b02c976cbecce10cdeff
Marilyn Negrete <negrete.aguilar.m@gmail.com>
2018-10-09 15:05:04 -0500

    map and promises

--- walmartApi.js
+++ walmartApi.js
@@ -1,15 +1,16 @@
 const fetch = require('node-fetch');
 
 fetch('http://api.walmartlabs.com/v1/items?ids=19336123,19336123&apiKey=dkh7e6ex762zpusgq8cqf47k')
-  .then(response => {
-      this.items = response
-      console.log(this.items)
-  });
+  .then(response => response.json())
+  .then(myObj => {
+    const allItemId = myObj.items
+    return allItemId.map(item => console.log(item.itemId))
+  })
 
 fetch('http://api.walmartlabs.com/v1/items?ids=12417832,19336123&apiKey=dkh7e6ex762zpusgq8cqf47k')
   .then(response => response.json())
   .then(myJson => {
-    console.log('**************')
-    console.log((myJson))
+    const allItems = myJson.items
+    return allItems.map(item => console.log(item.name, '<<<<----------'))
   })
-  .catch(err => console.log(err))
+  .catch(err => err)
