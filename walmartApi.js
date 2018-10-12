ad2238ea2d796f15a08513bcc71bd22fda8475b7
Marilyn Negrete <negrete.aguilar.m@gmail.com>
2018-10-12 18:02:11 -0500

    add postRequest.js

--- walmartApi.js
+++ walmartApi.js
@@ -1,11 +1,18 @@
 const fetch = require('node-fetch');
 
-fetch('http://api.walmartlabs.com/v1/items?ids=19336123,19336123&apiKey=dkh7e6ex762zpusgq8cqf47k')
+const container = []
+
+const p = fetch('http://api.walmartlabs.com/v1/items?ids=19336123,19336123&apiKey=dkh7e6ex762zpusgq8cqf47k')
   .then(response => response.json())
   .then(myObj => {
     const allItemId = myObj.items
-    return allItemId.map(item => console.log(item.itemId))
+    allItemId.map(item => console.log(item.itemId, '---aqui---'))
+        return allItemId.map(item => container.push(item.itemId))
   })
+
+
+console.log(p, '***************')
+
 
 fetch('http://api.walmartlabs.com/v1/items?ids=12417832,19336123&apiKey=dkh7e6ex762zpusgq8cqf47k')
   .then(response => response.json())
